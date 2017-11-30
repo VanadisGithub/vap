8d4d16596b337e45916e3f2496f68f6d8bf7469e
yaoyuan <yaoyuan@shangluo.com>
2017-11-30 22:12:20 +0800

    Initial commit

---
+++ src/main/java/com/vanadis/vap/VapApplication.java
@@ -0,0 +1,12 @@
+package com.vanadis.vap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VapApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vanadis/vap/VapApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vanadis.vap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VapApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
