44397d5e8e717e53b078ac8d79294de290166a9b
yaoyuan <yaoyuan@shangluo.com>
2018-01-10 19:33:59 +0800

    refix

---
+++ src/main/java/com/vanadis/vap/domain/User.java
@@ -0,0 +1,27 @@
+package com.vanadis.vap.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String userName;
+    @Column(nullable = false)
+    private String passWord;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = true, unique = true)
+    private String nickName;
+    @Column(nullable = false)
+    private String regTime;
+
+}
