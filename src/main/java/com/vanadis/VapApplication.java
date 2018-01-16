de4992c7d2fb1a672b423e70cdc46ca77536bd07
yaoyuan <yaoyuan@shangluo.com>
2018-01-16 15:33:43 +0800

    refix

--- src/main/java/com/vanadis/VapApplication.java
+++ src/main/java/com/vanadis/VapApplication.java
@@ -1,11 +1,13 @@
 package com.vanadis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling//增加支持定时任务的注解
+@MapperScan("com.vanadis.mapper")
 public class VapApplication {
 
 	public static void main(String[] args) {
