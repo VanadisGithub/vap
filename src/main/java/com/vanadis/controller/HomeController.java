7ec32bc19f3cd0f15d7c1dd1a1f8c76a5b430038
yaoyuan <yaoyuan@shangluo.com>
2018-01-16 15:32:46 +0800

    refix

--- src/main/java/com/vanadis/controller/HomeController.java
+++ src/main/java/com/vanadis/controller/HomeController.java
@@ -1,17 +1,33 @@
 package com.vanadis.controller;
 
+import com.vanadis.entity.User;
+import com.vanadis.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/home")
-public class HomeController extends BaseController{
+public class HomeController extends BaseController {
+
+    @Autowired
+    private UserMapper userMapper;
 
     @RequestMapping("")
     public ModelAndView index() {
         ModelAndView modelAndView = new ModelAndView("/home");
         return modelAndView;
+    }
+
+    @RequestMapping("/getUser")
+    public void getUser() throws Exception {
+        //userMapper.insert(new User("Vanadis", "ling123427", "872671438@qq.com", "Vanadis", "0"));
+        //List<User> users = userMapper.getAll();
+        User user = userMapper.getOne(1L);
+        System.out.println(user.toString());
     }
 
 }
