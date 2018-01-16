194c94cabe2f27740e86d73aa950fa86328bbdf6
yaoyuan <yaoyuan@shangluo.com>
2018-01-16 15:34:57 +0800

    refix

---
+++ src/main/java/com/vanadis/controller/BaseController.java
@@ -0,0 +1,13 @@
+package com.vanadis.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+public class BaseController {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+}
---
+++ src/main/java/com/vanadis/mapper/UserMapper.java
@@ -0,0 +1,36 @@
+package com.vanadis.mapper;
+
+import com.vanadis.entity.User;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public interface UserMapper {
+
+    @Select("SELECT * FROM user")
+    @Results({
+            @Result(property = "userName",  column = "user_name"),
+            @Result(property = "passWord", column = "pass_word"),
+            @Result(property = "nickName", column = "nick_name"),
+            @Result(property = "regTime", column = "reg_time")
+    })
+    List<User> getAll();
+
+    @Select("SELECT * FROM user WHERE id = #{id}")
+    @Results({
+            @Result(property = "userName",  column = "user_name"),
+            @Result(property = "passWord", column = "pass_word"),
+            @Result(property = "nickName", column = "nick_name"),
+            @Result(property = "regTime", column = "reg_time")
+    })
+    User getOne(Long id);
+
+    @Insert("INSERT INTO user(user_name,pass_word,email,nick_name,reg_time) VALUES(#{userName}, #{passWord}, #{email}, #{nickName}, #{regTime})")
+    void insert(User user);
+
+    @Delete("DELETE FROM user WHERE id =#{id}")
+    void delete(Long id);
+
+}
