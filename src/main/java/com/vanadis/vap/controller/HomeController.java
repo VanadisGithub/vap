06d634c06ea1ca46b5eacdbaff151025f5655c8e
vanadis <v.anadis@outlook.com>
2018-02-08 10:52:51 +0800

    refix

--- src/main/java/com/vanadis/vap/controller/HomeController.java
+++ src/main/java/com/vanadis/vap/controller/HomeController.java
@@ -2,11 +2,12 @@ package com.vanadis.vap.controller;
 
 import com.vanadis.vap.model.User;
 import com.vanadis.vap.model.UserMapper;
-import com.vanadis.vap.action.AutoCodeAction;
 import com.vanadis.vap.until.EmailUtils;
 import com.vanadis.vap.until.HttpUtils;
 import org.apache.http.HttpHost;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,6 +18,8 @@ public class HomeController extends BaseController {
 
     @Autowired
     private UserMapper userMapper;
+
+    String qqIcon = "http://q1.qlogo.cn/g?b=qq&nk=824503172&s=140&t=1368862220";
 
     @RequestMapping("")
     public ModelAndView index() {
@@ -48,10 +51,15 @@ public class HomeController extends BaseController {
         return resultStr;
     }
 
+    @Autowired
+    private JavaMailSender mailSender; //自动注入的Bean
+
+    @Value("${spring.mail.username}")
+    private String Sender; //读取配置文件中的参数
+
     @RequestMapping("test")
-    public void test() throws Exception {
-        String qqIcon = "http://q1.qlogo.cn/g?b=qq&nk=824503172&s=140&t=1368862220";
-        new EmailUtils().sendSimpleMail();
+    public void test() {
+        EmailUtils.send(mailSender, Sender, "872671438@qq.com", "测试", "测试内容");
     }
 
 }
